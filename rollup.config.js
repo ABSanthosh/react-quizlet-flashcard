21f44b2b76ba18b5976d7f0aba2de5592ba36a62
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-24 01:15:02 +0530

    refactor(Rollup): autoprefixer

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import image from "@rollup/plugin-image";
+import autoprefixer from "autoprefixer";
 import { terser } from "rollup-plugin-terser";
 
 export default [
@@ -21,7 +22,7 @@ export default [
     ],
     plugins: [
       postcss({
-        plugins: [],
+        plugins: [autoprefixer()],
         minimize: true,
       }),
       babel({
