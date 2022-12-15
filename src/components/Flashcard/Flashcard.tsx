4bfff078046caa04ac687314b380409a828af962
ABSanthosh <a.b.santhosh02@gmail.com>
2022-12-16 02:09:56 +0530

    feat: better flashcard component

--- src/components/Flashcard/Flashcard.tsx
+++ src/components/Flashcard/Flashcard.tsx
@@ -1,17 +1,166 @@
-import React from "react";
-import PropTypes from "prop-types";
-//import { Test } from './Flashcard.styles';
+import { useState } from "react";
+import "./Flashcard.scss";
 
-const Flashcard = (props) => (
-  <div className="FlashcardWrapper">Test content</div>
-);
+// added
+// manualFlipRef?: React.MutableRefObject<() => void>;
+// borderRadius?: string;
+// jsx elements for frontHTML and backHTML
 
-Flashcard.propTypes = {
-  // bla: PropTypes.string,
-};
+// removed
+// default styles like padding, border radius and flex alignment for content
 
-Flashcard.defaultProps = {
-  // bla: 'test',
-};
+interface FlashcardProps {
+  /**
+   * HTML string or JSX element to be displayed on the front of the card
+   */
+  frontHTML: string | JSX.Element;
+  /**
+   * CSS styles to be applied to the front side of the card
+   */
+  frontCardStyle?: React.CSSProperties;
+  /**
+   *  CSS styles to be applied to the content of the front side of the card
+   */
+  frontContentStyle?: React.CSSProperties;
+  /**
+   * HTML string or JSX element to be displayed on the back of the card
+   */
+  backHTML: string | JSX.Element;
+  /**
+   * CSS styles to be applied to the back side of the card
+   */
+  backCardStyle?: React.CSSProperties;
+  /**
+   * CSS styles to be applied to the content of the back side of the card
+   */
+  backContentStyle?: React.CSSProperties;
+  /**
+   * CSS class to be applied to the wrapper div
+   */
+  className?: string;
+  /**
+   * CSS height of the wrapper div
+   */
+  height?: string;
+  /**
+   * CSS border-radius of the wrapper div
+   */
+  borderRadius?: string;
+  /**
+   * CSS width of the wrapper div
+   */
+  width?: string;
+  /**
+   * Boolean to reset the state of the card
+   */
+  resetState?: boolean;
+  /**
+   * CSS styles to be applied to the wrapper div
+   */
+  style?: React.CSSProperties;
+  /**
+   *  Callback function to be called when the card is flipped
+   */
+  onCardFlip?: (state: boolean) => void;
+  /**
+   * when passed with a ref, ref.current object will contain reference to `flipCard()` function
+   */
+  manualFlipRef?: React.MutableRefObject<() => void>;
+}
+
+function Flashcard({
+  frontHTML,
+  frontCardStyle,
+  frontContentStyle,
+  backHTML,
+  backCardStyle,
+  backContentStyle,
+  className = "",
+  style,
+  height,
+  borderRadius,
+  width,
+  resetState = false,
+  onCardFlip = (state = false) => {},
+  manualFlipRef = null,
+}: FlashcardProps) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function onManualFlip() {
+    setIsFlipped(!isFlipped);
+    onCardFlip(!isFlipped);
+  }
+  if (manualFlipRef) manualFlipRef.current = onManualFlip;
+
+  return (
+    <div
+      className={`FlashcardWrapper ${className}`}
+      style={{
+        height: height,
+        width: width,
+        ...style,
+      }}
+    >
+      <div
+        className={`FlashcardWrapper__item ${
+          isFlipped ? "FlashcardWrapper__item--flip" : ""
+        }`}
+        style={{
+          borderRadius: borderRadius,
+        }}
+        onClick={() => {
+          if (manualFlipRef) return;
+          setIsFlipped(!isFlipped);
+          onCardFlip(!isFlipped);
+        }}
+      >
+        <div
+          className="FlashcardWrapper__item--front"
+          style={{
+            ...frontCardStyle,
+            cursor: manualFlipRef ? "default" : "pointer",
+          }}
+        >
+          {typeof frontHTML !== "string" ? (
+            <div
+              className="FlashcardWrapper__item--content"
+              style={frontContentStyle}
+            >
+              {frontHTML}
+            </div>
+          ) : (
+            <div
+              className="FlashcardWrapper__item--content"
+              dangerouslySetInnerHTML={{ __html: frontHTML }}
+              style={frontContentStyle}
+            />
+          )}
+        </div>
+        <div
+          className="FlashcardWrapper__item--back"
+          style={{
+            ...backCardStyle,
+            cursor: manualFlipRef ? "default" : "pointer",
+          }}
+        >
+          {typeof backHTML !== "string" ? (
+            <div
+              className="FlashcardWrapper__item--content"
+              style={backContentStyle}
+            >
+              {backHTML}
+            </div>
+          ) : (
+            <div
+              className="FlashcardWrapper__item--content"
+              dangerouslySetInnerHTML={{ __html: backHTML }}
+              style={backContentStyle}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Flashcard;
--- src/stories/Flashcard.stories.tsx
+++ src/stories/Flashcard.stories.tsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
 import { storiesOf } from "@storybook/react";
 const stories = storiesOf("Flashcard", module);
 import "./storyStyle.scss";
 
 import Flashcard from "../components/Flashcard/Flashcard";
 
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        padding: "10px",
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
 stories.add("default", () => {
+  const flipRef = useRef(null);
   return (
     <div className="storyContainer">
       <Flashcard
-      // style={{
-      //   margin: "auto",
-      // }}
-      // frontHTML="Front"
-      // backHTML="<h1>Back</h1>"
+        manualFlipRef={flipRef}
+        backCardStyle={{ backgroundColor: "red" }}
+        backContentStyle={{ color: "white", display: "block", padding: "0px" }}
+        borderRadius="10px"
+        backHTML={
+          <Button
+            onClick={() => {
+              console.log("hi");
+            }}
+          >
+            Click Me
+          </Button>
+        }
+        frontHTML="<h1>Back</h1>"
+        onCardFlip={(state) => {
+          console.log(state);
+        }}
       />
+      <button
+        onClick={() => {
+          flipRef.current();
+        }}
+      >
+        Flip
+      </button>
     </div>
   );
 });
